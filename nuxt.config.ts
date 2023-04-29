b5c613ff2e551faf8f996060a28ff3b526e07aac
Wesley Janse <wesleyjanse@gmail.com>
2023-04-29 15:38:22 +0200

    Update linting config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
+  components: [
+    {
+      path: "~/components",
+      extensions: [".vue"],
+      global: true,
+    },
+  ],
 });
